6b2a684cf1a582dc2485d98cf247cb5acd1a4792
Dario Mauri <dario.mauri_ext@zooplus.com>
2024-09-12 15:54:25 +0200

    Base application structure

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Ichiro walks
+
+Web app to track the walks of our dog Ichiro
+
+## Environment variables
+
+- `PORT`: server port on which to run the app (default: `8084`)
+- `PROFILE`: spring profile to apply (default: `prod`)
+    - `dev`: vaadin production mode is off
+    - `prod`: vaadin production mode is on
---
+++ src/main/java/com/dariom/ichirowalks/Application.java
@@ -0,0 +1,26 @@
+package com.dariom.ichirowalks;
+
+import static com.vaadin.flow.theme.lumo.Lumo.DARK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.AppShellSettings;
+import com.vaadin.flow.theme.Theme;
+
+@Push
+@SpringBootApplication
+@Theme(value = "my-theme", variant = DARK)
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    @Override
+    public void configurePage(AppShellSettings settings) {
+        settings.addFavIcon("icon", "/icons/favicon.png", "256x256");
+    }
+}
