8891a8f5ae2bc2fd14bff5932c7b934bcd3fa496
Dario Mauri <dario.mauri_ext@zooplus.com>
2024-09-17 09:11:42 +0200

    Display time-zone data to debug if server or client is used

--- src/main/java/com/dariom/ichirowalks/view/component/WalkRecorder.java
+++ src/main/java/com/dariom/ichirowalks/view/component/WalkRecorder.java
@@ -8,7 +8,10 @@ import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 public class WalkRecorder extends VerticalLayout {
@@ -19,6 +22,8 @@ public class WalkRecorder extends VerticalLayout {
     private final Button backButton;
 
     public WalkRecorder(IchiroWalkService ichiroWalkService) {
+        showTimeZones();
+
         this.ichiroWalkService = ichiroWalkService;
         setPadding(false);
 
@@ -47,5 +52,30 @@ public class WalkRecorder extends VerticalLayout {
     private void handleBack() {
         ichiroWalkService.updateBackAt(LocalDateTime.now());
         ComponentUtil.fireEvent(UI.getCurrent(), new RegisterWalkEvent(backButton));
+    }
+
+    // TODO remove
+    private void showTimeZones() {
+        var defaultZone = Clock.systemDefaultZone();
+        var UTCzone = Clock.systemUTC();
+
+        var defaultNow = LocalDateTime.now(defaultZone);
+        var utcNow = LocalDateTime.now(UTCzone);
+        var instant = Instant.now();
+
+        var zoneTxt = new TextField("Default time-zone: ", defaultZone.toString(), "");
+        zoneTxt.setWidthFull();
+        zoneTxt.setReadOnly(true);
+        var defaultTxt = new TextField("Default now: ", defaultNow.toString(), "");
+        defaultTxt.setWidthFull();
+        defaultTxt.setReadOnly(true);
+        var utcTxt = new TextField("UTC now: ", utcNow.toString(), "");
+        utcTxt.setWidthFull();
+        utcTxt.setReadOnly(true);
+        var instantTxt = new TextField("Instant now: ", instant.toString(), "");
+        instantTxt.setWidthFull();
+        instantTxt.setReadOnly(true);
+
+        add(zoneTxt, defaultTxt, utcTxt, instantTxt);
     }
 }
